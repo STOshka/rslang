0ab5cafd088d8e4f05815d9e0736dcbba2c2e49e
STOshka <34401821+STOshka@users.noreply.github.com>
2022-08-22 23:10:52 +0300

    refactor: move audiosvg to helpers.ts

--- src/pages/audioChallenge/index.ts
+++ src/pages/audioChallenge/index.ts
@@ -1,6 +1,6 @@
 import API from '../../application/api';
 import { Constants } from '../../utils/constants';
-import { shuffle } from '../../utils/helpers';
+import { getAudioSvg, shuffle } from '../../utils/helpers';
 import { IWordsInf } from '../../utils/types';
 import BasePage from '../basePage';
 import Answer from './answer';
@@ -18,7 +18,7 @@ class AudioChallengePage extends BasePage {
     async init() {
         const BODY = document.querySelector('body') as HTMLElement;
         BODY.innerHTML = `<main class="audio__challenge__main">
-            <div class="audio__challenge__svg">${this.getAudioSvg()}</div>
+            <div class="audio__challenge__svg">${getAudioSvg()}</div>
             <div class="audio__challenge__answers"></div>
             <div class="audio__challenge__button">I don't know</div>
         </main>`;
@@ -41,12 +41,6 @@ class AudioChallengePage extends BasePage {
             (document.querySelector('.audio__challenge__button') as HTMLElement).innerHTML = '-->';
             this.answered = true;
         }
-    }
-    getAudioSvg(): string {
-        return `<svg viewBox="0 0 24 24" aria-hidden="true">
-            <path d="M3 9v6h4l5 5V4L7 9H3zm13.5 3c0-1.77-1.02-3.29-2.5-4.03v8.05c1.48-.73 2.5-2.25 2.5-4.02zM14
-            3.23v2.06c2.89.86 5 3.54 5 6.71s-2.11 5.85-5 6.71v2.06c4.01-.91 7-4.49 7-8.77s-2.99-7.86-7-8.77z"></path>
-        </svg>`;
     }
     async generateWord(): Promise<void> {
         (document.querySelector('.audio__challenge__button') as HTMLElement).innerHTML = `I don't know`;

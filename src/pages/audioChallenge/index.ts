85215bfc7977824e8b8bd02973d215e58d064509
STOshka <34401821+STOshka@users.noreply.github.com>
2022-08-22 23:37:31 +0300

    feat: add generate words and move to next after answering

--- src/pages/audioChallenge/index.ts
+++ src/pages/audioChallenge/index.ts
@@ -9,31 +9,36 @@ import './index.css';
 class AudioChallengePage extends BasePage {
     words: IWordsInf[] = [];
     answers: Answer[] = [];
+    wordIndex = -1;
     correctAnswer: Answer | undefined;
     answered = true;
     constructor(api: API) {
         super(api);
     }
-    async init() {
+    async init(): Promise<void> {
         const BODY = document.querySelector('body') as HTMLElement;
         BODY.innerHTML = `<main class="audio__challenge__main">
             <div class="audio__challenge__svg">${getAudioSvg()}</div>
             <div class="audio__challenge__answers"></div>
             <div class="audio__challenge__button">I don't know</div>
         </main>`;
+        (document.querySelector('.audio__challenge__svg') as HTMLElement).addEventListener('click', () => {
+            const audio = new Audio(`${Constants.URL}${this.words[0].audio}`);
+            audio.play();
+        });
         (document.querySelector('.audio__challenge__answers') as HTMLElement).addEventListener('click', (e: Event) => {
             if (this.answered) return;
             const target: HTMLElement = (e.target as HTMLElement).closest('.audio__challenge__answer') as HTMLElement;
             const answer = this.answers.find((el) => el.node === target);
             answer && this.checkAnswer(answer);
         });
-        (document.querySelector('.audio__challenge__svg') as HTMLElement).addEventListener('click', () => {
-            const audio = new Audio(`${Constants.URL}${this.words[0].audio}`);
-            audio.play();
-        })
-        this.generateWord();
+        (document.querySelector('.audio__challenge__button') as HTMLElement).addEventListener('click', () => {
+            this.nextWord();
+        });
+        await this.generateWords();
+        this.nextWord();
     }
-    checkAnswer(answer: Answer) {
+    checkAnswer(answer: Answer): void {
         if (answer === this.correctAnswer) {
             answer.markAsCorrect();
         } else {
@@ -43,22 +48,28 @@ class AudioChallengePage extends BasePage {
         (document.querySelector('.audio__challenge__button') as HTMLElement).innerHTML = '-->';
         this.answered = true;
     }
-    async generateWord(): Promise<void> {
-        (document.querySelector('.audio__challenge__button') as HTMLElement).innerHTML = `I don't know`;
-        this.words = await this.api.getWordList(0, 0);
-        const correctAnswer = this.words[0].wordTranslate;
-        const possibleAnswers = this.words
-            .filter((el) => el.wordTranslate !== correctAnswer)
-            .map((el) => el.wordTranslate);
-        if (possibleAnswers) {
+    async generateWords(): Promise<void> {
+        const words = await this.api.getWordList(0, 0);
+        this.words = shuffle(words);
+    }
+    nextWord(): void {
+        if (this.words && this.wordIndex < this.words.length - 1) {
+            this.wordIndex += 1;
+            console.log(this.wordIndex);
+            const correctAnswer = this.words[this.wordIndex].wordTranslate;
+            const possibleAnswers = this.words
+                .filter((el) => el.wordTranslate !== correctAnswer)
+                .map((el) => el.wordTranslate);
             this.answers = shuffle([
                 correctAnswer,
                 ...shuffle(possibleAnswers).slice(0, Constants.AUDIO_CHALLENGE_COUNT_ANSWERS - 1),
             ]).map((answer, index) => new Answer(index, answer, answer === correctAnswer));
             const answersDiv = document.querySelector('.audio__challenge__answers') as HTMLElement;
+            answersDiv.innerHTML = '';
             this.answers.forEach((answer) => answersDiv.append(answer.node));
             this.correctAnswer = this.answers.find((el) => el.isCorrect);
             this.answered = false;
+            (document.querySelector('.audio__challenge__button') as HTMLElement).innerHTML = `I don't know`;
         }
     }
 }

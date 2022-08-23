1742194804224f907033afc6b46632d6631f878d
Yuliya0503 <yuliyanarkevich0503@gmail.com>
2022-08-23 13:38:26 +0200

    feat: add sprintButton class

---
+++ src/pages/sprint/sprintButton.ts
@@ -0,0 +1,9 @@
+export default class SprintButton {
+    template: string;
+    constructor() {
+        this.template = `
+    <button type="button" class="sprint__button" id="sprint-yes">Yes</button>
+    <button type="button" class="sprint__button" id="sprint-no">No</button>
+    `;
+    }
+}

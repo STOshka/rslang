0135998e0d2a885cc40173784c13c4613a50ca63
Yuliya0503 <yuliyanarkevich0503@gmail.com>
2022-08-26 11:06:26 +0200

    feat: add answerCheck method

--- src/pages/sprint/index.ts
+++ src/pages/sprint/index.ts
@@ -1,33 +1,33 @@
 import API from '../../application/api';
 import BaseGamePage from '../baseGamePage';
-import { IWord, GameState, GameWordStatistic, ROUTES } from '../../utils/types';
-import {shuffle, createHTMLElement, getAudioSvg, inRange, randomInt, randomIntRange} from '../../utils/helpers';
-import { Constants } from '../../utils/constants';
- 
+import { IWord, GameState, GameWordStatistic } from '../../utils/types';
+import { shuffle, randomIntRange } from '../../utils/helpers';
+
 class SprintPage extends BaseGamePage {
-    
     words: IWord[] = [];
     wordIndex = -1;
     statistic: GameWordStatistic[] = [];
     gameState = GameState.StartScreen;
     group = -1;
     page = -1;
-    answer:boolean;
+    answer: boolean;
     time: number;
-   
+    score: number;
+
     constructor(api: API) {
         super(api);
         this.answer = false;
         this.time = 60;
-    };
+        this.score = 0;
+    }
 
-    init(query: URLSearchParams): void {
+    init(query: URLSearchParams) {
         super.init(query);
-    };
+    }
 
-    gameName(): string {
-        return 'СПРИНТ'; 
-    };
+    gameName() {
+        return 'СПРИНТ';
+    }
 
     async generateGame() {
         const words = await this.api.getWordList(this.group, this.page);
@@ -38,14 +38,15 @@ class SprintPage extends BaseGamePage {
         this.wordIndex = -1;
     }
 
-    timer(){
+    timer() {
         const setIntervalId = setInterval(() => {
             const timeSprint = document.querySelector('#sprint-time');
-            if ((this.time >= 0) && timeSprint) {
+            if (this.time >= 0 && timeSprint) {
                 (timeSprint as HTMLElement).innerText = ` Time: ${this.time}`;
                 this.time -= 1;
             } else {
                 clearInterval(setIntervalId);
+                this.endGame();
             }
             return this.time;
         }, 1000);
@@ -56,41 +57,38 @@ class SprintPage extends BaseGamePage {
         this.gameState = GameState.Question;
         const word = this.words[0];
         const randomWord = this.words[randomIntRange(0, 19)];
-        if(word.id === randomWord.id) this.answer = true; 
-        console.log(this.answer)
+        if (word.id === randomWord.id) this.answer = true;
         const MAIN = document.querySelector('.main') as HTMLElement;
         MAIN.innerHTML = `<main class="sprint__main">
             <div class="sprint__timer" id="sprint-time">${this.timer()}</div>
-            <div class="sprint__score"></div>
+            <div class="sprint__score">${this.score}</div>
             <div class="sprint__word">${word.word}</div>
             <div class="sprint__translate">${randomWord.wordTranslate}</div>
             <button class="sprint__button" type="button"id="sprint-yes">Верно</button>
             <button class="sprint__button" type="button"id="sprint-no">Неверно</button>
         </main>`;
-        
-    }   
-   
+        (MAIN.querySelector('#sprint-yes') as HTMLElement).addEventListener('click', () => {
+            this.answerCheck(true, word);
+            (MAIN.querySelector('.sprint__score') as HTMLElement).innerText = `${this.score}`;
+        });
+        (MAIN.querySelector('#sprint-no') as HTMLElement).addEventListener('click', () => {
+            this.answerCheck(false, word);
+            (MAIN.querySelector('.sprint__score') as HTMLElement).innerText = `${this.score}`;
+        });
+    }
+
+    answerCheck(bthAnswer: boolean, word: IWord) {
+        if (bthAnswer === this.answer) {
+            super.addWordstatistic(word, true);
+            this.score += 10;
+        } else {
+            super.addWordstatistic(word, false);
+        }
+    }
+
     endGame(): void {
         super.endGame();
     }
-    
-
-   /*
-
-
-    async addAnswerYes(right: IWordsInf[], info?: IWordsInf) {
-        const baseBill = 10;
-        if (info) {
-            right[right.length] = info;
-            this.score = String(baseBill + Number(this.score));
-        }
-    }
-
-    addAnswerNo(wrong: IWordsInf[], info?: IWordsInf) {
-        if (info) {
-            wrong[wrong.length] = info;
-        }
-    }*/
 }
 
 export default SprintPage;

df6eafb9833622f38db135d9f52965cc118434c3
Yuliya0503 <yuliyanarkevich0503@gmail.com>
2022-08-30 12:44:36 +0200

    feat: add team photos, make changes to webpack

--- src/pages/aboutTheTeam/constants.ts
+++ src/pages/aboutTheTeam/constants.ts
@@ -1,16 +1,15 @@
 export const INFORMATION = [
     {
         name: 'Alexandr Stoyanov',
-        img: 'https://avatars.githubusercontent.com/u/34401821?v=4',
+        img: './assets/Sasha.jpg',
         gitHubLink: 'https://github.com/stoshka',
         gitHub: 'STOshka',
         role: 'Team Lead',
-        work:
-            'Координировал команду. Разработал архитектуру приложения. Настроил backend. Разработал игру "Аудиовызов". Реализовал авторизацию.',
+        work: 'Координировал команду. Разработал архитектуру приложения. Настроил backend. Разработал игру "Аудиовызов". Реализовал авторизацию и статистику приложения.',
     },
     {
         name: 'Dzianis Valkovich',
-        img: 'https://avatars.githubusercontent.com/u/96067964?v=4',
+        img: './assets/Denis.jpg',
         gitHubLink: 'https://github.com/DenisWilk',
         gitHub: 'DenisWilk',
         role: 'Developer',
@@ -18,10 +17,10 @@ export const INFORMATION = [
     },
     {
         name: 'Yuliya Narkevich',
-        img: 'https://cdn.rs.school/yuliya0503.png?size=192',
+        img: './assets/Yuliya.jpg',
         gitHubLink: 'https://github.com/yuliya0503',
         gitHub: 'Yuliya0503',
         role: 'Developer',
-        work: 'Разработала игру "Спринт". Создала страницы "О команде" и "Игры".',
+        work: 'Разработала игру "Спринт". Создала страницы "О команде", "Статистика" и "Страницу ошибки".',
     },
 ];
